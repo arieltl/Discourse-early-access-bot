863ec08cf0383bcc33139fa0f6db31e637fd82d7
ariel leventhal <arielleventhal@gmail.com>
2022-09-08 20:26:09 -0300

    v1

---
+++ main.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from email import header
+import json
+from os import system
+import re
+import requests
+import dateutil.parser
+from  datetime import datetime
+import time
+
+with open("config.json") as file:
+	config = json.load(file)
+
+key = config["Api-Key"]	
+user = config["Api-Username"]
+url = config["base-url"]
+data = {"category_id":config["to-id"]}
+headers={"Api-Key":key,"Api-Username":user}
+response = requests.get(url+f"c/{config['from-slug']}/{config['from-id']}.json",headers=headers)
+
+for topic in response.json()["topic_list"]["topics"]:
+	date = dateutil.parser.isoparse(topic["created_at"])
+	now = datetime.now().astimezone()
+	delta = now-date
+	if delta.days>10:
+		requests.put(url+f"/t/-/{topic['id']}.json",headers=headers,data=data)
